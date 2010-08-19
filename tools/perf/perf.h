a3f4814444feafa7002d26f8302352870ac6cc9b
Bruce Ashfield <bruce.ashfield@windriver.com>
2010-08-19 11:24:45 -0400

    perf: force include of <stdbool.h>
    Large parts of perf use bool variables and assignments.
    Rather than depending on transient includes, we force it
    by including <stdbool.h> directly in perf.h
    
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- tools/perf/perf.h
+++ tools/perf/perf.h
@@ -3,6 +3,8 @@
 
 #include <asm/unistd.h>
 
+#include <stdbool.h>
+
 #if defined(__i386__)
 #define rmb()		asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
 #define cpu_relax()	asm volatile("rep; nop" ::: "memory");
