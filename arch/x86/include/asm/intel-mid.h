993d1919b48b0b6f5aa5f3d19bb76a965b0ad039
Dale Stimson <dale.b.stimson@intel.com>
2012-11-01 08:55:07 -0700

    intel-mid: Support msgbus addresses over 8 bits.
    The msgbus access functions in intel_mid.c supported only 8-bit
    msgbus addresses.  Add support for 32-bit addresses.
    
    Kernel 3.0 - http://android.intel.com:8080/72488
    Kernel 3.4 - http://android.intel.com:8080/91599
    
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- arch/x86/include/asm/intel-mid.h
+++ arch/x86/include/asm/intel-mid.h
@@ -28,18 +28,21 @@ static inline int get_gpio_by_name(const char *name) { return -1; }
 #endif
 
 /*
- * Access to message bus through these 2 registers
+ * Access to message bus through three registers
  * in CUNIT(0:0:0) PCI configuration space.
  * MSGBUS_CTRL_REG(0xD0):
  *   31:24	= message bus opcode
  *   23:16	= message bus port
- *   15:8	= message bus address
+ *   15:8	= message bus address, low 8 bits.
  *   7:4	= message bus byte enables
- * MSGBUS_DTAT_REG(0xD4):
+ * MSGBUS_CTRL_EXT_REG(0xD8):
+ *   31:8	= message bus address, high 24 bits.
+ * MSGBUS_DATA_REG(0xD4):
  *   hold the data for write or read
  */
 #define PCI_ROOT_MSGBUS_CTRL_REG	0xD0
 #define PCI_ROOT_MSGBUS_DATA_REG	0xD4
+#define PCI_ROOT_MSGBUS_CTRL_EXT_REG	0xD8
 #define PCI_ROOT_MSGBUS_READ		0x10
 #define PCI_ROOT_MSGBUS_WRITE		0x11
 #define PCI_ROOT_MSGBUS_DWORD_ENABLE	0xf0
@@ -55,8 +58,9 @@ extern int sfi_mrtc_num;
 extern struct sfi_rtc_table_entry sfi_mrtc_array[];
 extern u32 intel_mid_msgbus_read32_raw(u32 cmd);
 extern void intel_mid_msgbus_write32_raw(u32 cmd, u32 data);
-extern u32 intel_mid_msgbus_read32(u8 port, u8 addr);
-extern void intel_mid_msgbus_write32(u8 port, u8 addr, u32 data);
+extern u32 intel_mid_msgbus_read32(u8 port, u32 addr);
+extern void intel_mid_msgbus_write32(u8 port, u32 addr, u32 data);
+extern void register_rpmsg_service(char *name, int id, u32 addr);
 extern int sdhci_pci_request_regulators(void);
 
 /* OEMB table */
