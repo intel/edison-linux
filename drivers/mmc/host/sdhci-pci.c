8288105cf64d095d2defd43a3082df419f2041f6
Chuanxiao Dong <chuanxiao.dong@intel.com>
2013-07-17 14:07:10 +0800

    Panic: mmc: sdhci: implement panic ops for MFLD SDHCI driver
    This patch also contains two bug fixes for feature:
    1. mmc: panic: using the api provided by PMU driver in panic mode
    2. sdhci-pci: use X86_MDFLD to replace X86_MRST
    
    This patch will implement the panic ops for MFLD sdhci host driver.
    The original sdhci host driver was using interrupt mode which is not
    preferred in panic mode. So implemented another ops which will use
    polling mode instead
    
    Signed-off-by: Ning Feiyi <feiyix.ning@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- drivers/mmc/host/sdhci-pci.c
+++ drivers/mmc/host/sdhci-pci.c
@@ -30,6 +30,10 @@
 #include <asm/intel_scu_flis.h>
 #include <asm/intel_scu_pmic.h>
 
+#if defined(CONFIG_X86_MDFLD)
+#include <linux/intel_mid_pm.h>
+#endif
+
 #include "sdhci.h"
 
 /* Settle down values copied from broadcom reference design. */
@@ -330,6 +334,7 @@ static int mfd_emmc_probe_slot(struct sdhci_pci_slot *slot)
 	switch (slot->chip->pdev->device) {
 	case PCI_DEVICE_ID_INTEL_MFD_EMMC0:
 		mfd_emmc_mutex_register(slot);
+		sdhci_alloc_panic_host(slot->host);
 		break;
 	case PCI_DEVICE_ID_INTEL_MFD_EMMC1:
 		break;
@@ -1666,6 +1671,49 @@ static int sdhci_gpio_buf_check(struct sdhci_host *host, unsigned int clk)
 
 	return ret;
 }
+
+#if defined(CONFIG_X86_MDFLD)
+static int sdhci_pci_power_up_host(struct sdhci_host *host)
+{
+	int ret;
+	bool atomic_context;
+
+	/*
+	 * Since pmu_set_emmc_to_d0i0_atomic function can
+	 * only be used in atomic context, before call this
+	 * function, do a check first and make sure this function
+	 * is used in atomic context.
+	 */
+	atomic_context = (!preemptible() || in_atomic_preempt_off());
+
+	if (!atomic_context) {
+		pr_err("%s: not in atomic context!\n", __func__);
+		return -EPERM;
+	}
+
+	ret = pmu_set_emmc_to_d0i0_atomic();
+	if (ret) {
+		pr_err("%s: power up host failed\n", __func__);
+		return ret;
+	}
+
+	/*
+	 * after power up host, let's have a little test
+	 */
+	if (sdhci_readl(host, SDHCI_PRESENT_STATE) ==
+			0xffffffff) {
+		pr_err("%s: but power up failed\n",
+				__func__);
+		return -EPERM;
+	}
+
+	pr_info("%s: host controller power up is done\n", __func__);
+
+	return 0;
+}
+#else
+#define sdhci_pci_power_up_host        NULL
+#endif
 
 static const struct sdhci_ops sdhci_pci_ops = {
 	.enable_dma	= sdhci_pci_enable_dma,
