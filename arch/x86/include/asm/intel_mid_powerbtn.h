22043befe6b9b3da188c7ba9ac600a13d241231e
Wang Yiyang <yiyang.wang@intel.com>
2013-08-27 06:17:17 +0800

    powerbtn: port power button driver from K3.4
    Port cloverview and tangier power button driver from K3.4 to K3.10
    
    Signed-off-by: Wang Yiyang <yiyang.wang@intel.com>

---
+++ arch/x86/include/asm/intel_mid_powerbtn.h
@@ -0,0 +1,14 @@
+#ifndef __INTEL_MID_POWERBTN_H__
+#define __INTEL_MID_POWERBTN_H__
+
+struct intel_msic_power_btn_platform_data {
+	u32 pbstat;
+	u16 pb_level;
+	u16 irq_lvl1_mask;
+	int (*irq_ack)(struct intel_msic_power_btn_platform_data *);
+};
+
+#define MSIC_PB_LEN	1
+#define MSIC_PWRBTNM	(1 << 0)
+
+#endif
