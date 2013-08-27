c7d6c9933df11bf6ffbb6cf6a5ef504323371d53
Wang Yiyang <yiyang.wang@intel.com>
2013-08-27 14:31:09 +0800

    iio: port K3.4 iio APIs to K3.10
    port K3.4 iio APIs to K3.10
    
    Signed-off-by: Wang Yiyang <yiyang.wang@intel.com>

--- include/linux/iio/iio.h
+++ include/linux/iio/iio.h
@@ -287,6 +287,9 @@ struct iio_info {
 			int *val2,
 			long mask);
 
+	int (*read_all_raw)(struct iio_channel *chan,
+			int *val);
+
 	int (*write_raw)(struct iio_dev *indio_dev,
 			 struct iio_chan_spec const *chan,
 			 int val,
