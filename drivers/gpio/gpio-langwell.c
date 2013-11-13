6b206fad2d397d0dbdc5ce22c067c1f010f90565
Li, Ning <ning.li@intel.com>
2013-11-13 08:53:23 +0800

    gpio-langwell: fix KW issue of no return value
    lnw_gpio_set_pull() doesn't have return value if it passes all
    error condition. Fix this KW issue.
    
    Signed-off-by: Li, Ning <ning.li@intel.com>

--- drivers/gpio/gpio-langwell.c
+++ drivers/gpio/gpio-langwell.c
@@ -410,7 +410,7 @@ static int lnw_gpio_set_pull(struct gpio_chip *chip, unsigned gpio, int value)
 
 	flis_offset = lnw->get_flis_offset(gpio);
 	if (WARN(flis_offset == -EINVAL, "invalid pin %d\n", gpio))
-		return -1;
+		return -EINVAL;
 	if (flis_offset >= I2C_FLIS_START && flis_offset <= I2C_FLIS_END)
 		return 0;
 	flis_value = get_flis_value(flis_offset);
@@ -423,6 +423,8 @@ static int lnw_gpio_set_pull(struct gpio_chip *chip, unsigned gpio, int value)
 	}
 	flis_value |= PUPD_VAL_50K;
 	set_flis_value(flis_value, flis_offset);
+
+	return 0;
 }
 
 static void lnw_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
