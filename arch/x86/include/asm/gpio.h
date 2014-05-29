6e63ee297ddd5195e547231abea48407d5b1de00
Dan O'Donovan <danielx.o'donovan@intel.com>
2014-05-29 16:27:22 +0200

    gpio: increase system limit of 256 GPIOs for Edison platform
    The kernel GPIO subsystem imposes a default limit of 256 GPIO pins
    for the system.  However, the Edison Arduino platform has 262 GPIOs.
    This patch follows the arch/arm example to specify a higher limit for
    specific platforms.
    
    Signed-off-by: Dan O'Donovan <danielx.o'donovan@intel.com>

--- arch/x86/include/asm/gpio.h
+++ arch/x86/include/asm/gpio.h
@@ -1,4 +1,27 @@
+#ifndef _ARCH_X86_GPIO_H
+#define _ARCH_X86_GPIO_H
+
+#if CONFIG_ARCH_HAVE_CUSTOM_GPIO_H
+
+#if CONFIG_ARCH_NR_GPIO > 0
+#define ARCH_NR_GPIOS CONFIG_ARCH_NR_GPIO
+#endif
+
+#include <asm-generic/gpio.h>
+
+/* The trivial gpiolib dispatchers */
+#define gpio_get_value	__gpio_get_value
+#define gpio_set_value	__gpio_set_value
+#define gpio_cansleep	__gpio_cansleep
+#define gpio_to_irq	__gpio_to_irq
+
+#else /* ! CONFIG_ARCH_HAVE_CUSTOM_GPIO_H */
+
 #ifndef __LINUX_GPIO_H
 #warning Include linux/gpio.h instead of asm/gpio.h
 #include <linux/gpio.h>
 #endif
+
+#endif /* ! CONFIG_ARCH_HAVE_CUSTOM_GPIO_H */
+
+#endif /* _ARCH_X86_GPIO_H */
