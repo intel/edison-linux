79271a4f5f9afd8c13bd41a362745e7915733676
Michael Soares <michaelx.soares@intel.com>
2015-01-23 10:04:25 +0100

    Audio: select a sound card from cmdline
    The audio subsystem has to allow the user to select
    an audio codec with the command line in order to avoid
    rebuilding the kernel. Currently, the dummy codec and
    the wm8958 are selectable.
    
    This patch aims to add a module param in the platform audio
    driver to install the selected sound card according to the
    codec name provided by the command line.
    
    Signed-off-by: Michael Soares <michaelx.soares@intel.com>

--- arch/x86/platform/intel-mid/board.c
+++ arch/x86/platform/intel-mid/board.c
@@ -157,10 +157,10 @@ struct devs_id __initconst device_ids[] = {
 						&ipc_device_handler},
 	{"i2c_pmic_adap", SFI_DEV_TYPE_IPC, 1, &mrfl_pmic_i2c_platform_data,
 						&ipc_device_handler},
-	{"mrfld_wm8958", SFI_DEV_TYPE_IPC, 1, &merfld_wm8958_audio_platform_data,
+	{"mrfld_sst", SFI_DEV_TYPE_IPC, 1, &mrfld_sst_audio_platform_data,
 						&ipc_device_handler},
 	{"soc_thrm", SFI_DEV_TYPE_IPC, 1, &no_platform_data,
 						&soc_thrm_device_handler},
-	{"wm8958", SFI_DEV_TYPE_I2C, 0, &wm8994_platform_data, NULL},
+	{"mrfld_codec", SFI_DEV_TYPE_I2C, 0, &wm8994_platform_data, NULL},
 	{},
 };
